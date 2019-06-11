d47bcf0c99573367d9ab11cd3b3d795cc74f5757
Victor Hugo Olvera Cruz <hugotb88@gmail.com>
2019-06-11 18:24:43 -0500

    Initial Commit

---
+++ src/main/java/com/freelanceando/gastalon/GastalonApplication.java
@@ -0,0 +1,13 @@
+package com.freelanceando.gastalon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastalonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastalonApplication.class, args);
+    }
+
+}
