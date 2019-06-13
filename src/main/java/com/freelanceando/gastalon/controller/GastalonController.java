bafeac3426376097842c3761064d213a309e7167
Victor Hugo Olvera Cruz <victor olvera@TDG-MBL-GDL660.local>
2019-06-13 17:34:12 -0500

    Added File for endpoints for Gastalon

---
+++ src/main/java/com/freelanceando/gastalon/controller/GastalonController.java
@@ -0,0 +1,20 @@
+package com.freelanceando.gastalon.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Endpoints for Gastalon
+ */
+@RestController
+public class GastalonController {
+
+    @PostMapping("/add")
+    public void addMovement(@RequestBody String movement){
+        //TODO Logic to insert a movement in the DB
+    }
+
+
+}
